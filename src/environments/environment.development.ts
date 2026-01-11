d4c4061d0954dca083d502167013ccd36d2df8a8
AFOLABI GMATT MATTHEW <gmattworld@gmail.com>
2026-01-11 22:54:58 +0100

    Update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,5 @@
 export const environment = {
-  production: false,
-  googleMapsApiKey: 'AIzaSyA1kJyimQbtmc0MLZ-o0IzBLQZAt28MKoE',
-  API_BASE_URL: 'http://127.0.0.1:5074/api',
-  TENANT_PUBLIC_KEY: '01K5MVZDTVDGJEBRH9Y5FMSRG6',
-  TENANT_SECRET_KEY: 'af347116-d602-4303-9cf8-7f3c0785f526',
+  PRODUCTION: false,
+  GOOGLE_MAPS_API_KEY: 'AIzaSyA1kJyimQbtmc0MLZ-o0IzBLQZAt28MKoE',
+  API_BASE_URL: 'http://127.0.0.1:5074/api', // https://eifpkr3kbwzdvxnh4hipydzchq0mikjy.lambda-url.eu-central-1.on.aws/
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
-  production: false,
-  googleMapsApiKey: 'AIzaSyA1kJyimQbtmc0MLZ-o0IzBLQZAt28MKoE',
-  API_BASE_URL: 'https://edmiulioah2abyv3jtnc6jod4q0liybk.lambda-url.eu-central-1.on.aws/api',
-  TENANT_PUBLIC_KEY: '01K5MVZDTVDGJEBRH9Y5FMSRG6',
-  TENANT_SECRET_KEY: 'af347116-d602-4303-9cf8-7f3c0785f526',
+  PRODUCTION: false,
+  GOOGLE_MAPS_API_KEY: 'AIzaSyA1kJyimQbtmc0MLZ-o0IzBLQZAt28MKoE',
+  API_BASE_URL: 'https://zrfsiuoaqyoqd43i7oegd6lfsq0rihzn.lambda-url.eu-central-1.on.aws/api', 
 };
